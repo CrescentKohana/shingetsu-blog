9b4e5d244f6178b932899002c0e548f163358eeb
Marko Leinikka <mleinikka@gmail.com>
2021-08-31 18:29:05 +0300

    Small fixes

--- frontend/components/Seo.tsx
+++ frontend/components/Seo.tsx
@@ -15,7 +15,6 @@ const Seo = ({ seo }: SeoProps) => {
     ...seo,
   }
 
-  // TODO: "Guides | undefined" in Discord preview (https://luukuton.fi/category/guides)
   const fullSeo = {
     ...seoWithDefaults,
     metaTitle: `${seoWithDefaults.metaTitle} | ${global.siteName}`,
