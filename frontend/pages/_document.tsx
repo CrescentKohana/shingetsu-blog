7321a95a07a3aa7ba33785babec66e3a17140301
Marko Leinikka <mleinikka@gmail.com>
2021-09-28 01:58:14 +0300

    Fix Plausible integration

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,6 +1,8 @@
+// Fixed in Next.js 11.1.3-canary.7. Upgrade when it hits stable.
+// eslint-disable-next-line @next/next/no-document-import-in-page
 import Document, { Head, Html, Main, NextScript } from "next/document"
 
-// TODO: Possibly serve these scripts and stylesheets locally.
+// TODO: Possibly serve uikit scripts and stylesheets locally.
 
 class MyDocument extends Document {
   render() {
@@ -17,13 +19,14 @@ class MyDocument extends Document {
           <script async src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.7.2/js/uikit.min.js" />
           <script async src="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit-icons.min.js" />
           <script async src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.7.2/js/uikit.js" />
+          <script async defer data-domain="luukuton.fi" src="https://plausible.luukuton.fi/js/plausible.js"></script>
         </Head>
         <body>
           <Main />
           <NextScript />
-          <script async defer data-domain="luukuton.fi" src="https://plausible.luukuton.fi/js/plausible.js"></script>
           <noscript>
-            <img src="https://shy.luukuton.fi/ingress/ffa329be-5da8-4509-b9fd-1fdd5f37c4dd/pixel.gif" />
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img alt="shy pixel" src="https://shy.luukuton.fi/ingress/ffa329be-5da8-4509-b9fd-1fdd5f37c4dd/pixel.gif" />
           </noscript>
           <script defer src="https://shy.luukuton.fi/ingress/ffa329be-5da8-4509-b9fd-1fdd5f37c4dd/script.js"></script>
         </body>
