b6d6df65523ca11976c5d8aed4c294e0c8d1dc1f
Marko Leinikka <Luukuton@users.noreply.github.com>
2021-11-13 05:47:32 +0200

    Add video support and shuffle media

--- frontend/components/Slideshow.tsx
+++ frontend/components/Slideshow.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import { sauce } from "../lib/api"
+import { shuffle } from "../lib/helpers"
 import { getMedia } from "../lib/media"
 import { Media, SlideCategory } from "../types"
 
@@ -13,20 +14,24 @@ interface SlideshowProps {
 }
 
 const Slideshow = ({ items, category, slideshowProps, itemProps, slideshowClass, nav }: SlideshowProps) => {
-  console.log(category === SlideCategory.art)
+  const shuffled = shuffle(items) as Media[]
   return (
     <div data-uk-slideshow={slideshowProps} className={slideshowClass}>
       <div className="uk-position-relative uk-visible-toggle" tabIndex={0}>
         <ul className="uk-slideshow-items">
-          {items.map((item, i) => (
+          {shuffled.map((item, i) => (
             <li key={i} className={itemProps}>
-              <Image
-                src={getMedia(item)}
-                alt={item.alternativeText}
-                layout="fill"
-                objectFit="cover"
-                priority={i === 0}
-              />
+              {category === SlideCategory.video ? (
+                <video src={getMedia(item)} loop muted playsInline uk-video="autoplay: inview"></video>
+              ) : (
+                <Image
+                  src={getMedia(item)}
+                  alt={item.alternativeText}
+                  layout="fill"
+                  objectFit="cover"
+                  priority={i === 0}
+                />
+              )}
               {(item.caption || category === SlideCategory.art) && (
                 <div
                   style={{ padding: 15 }}
