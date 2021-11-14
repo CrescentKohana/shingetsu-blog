e16e7a5d7e039b3a485db4cdd34fa1c72a057f2e
Marko Leinikka <Luukuton@users.noreply.github.com>
2021-11-15 00:30:03 +0200

    Make links optional for tags

--- frontend/components/Tags.tsx
+++ frontend/components/Tags.tsx
@@ -4,10 +4,11 @@ import { Tag } from "../types"
 
 interface TagsProps {
   tags: Tag[]
+  links?: boolean
   onClick?: (tag: Tag, add: boolean) => void
 }
 
-const Tags = ({ tags, onClick }: TagsProps) => {
+const Tags = ({ tags, links, onClick }: TagsProps) => {
   const [tagsState, setTagsState] = useState<number[]>([])
 
   // Basic links without fancy filtering.
@@ -15,10 +16,14 @@ const Tags = ({ tags, onClick }: TagsProps) => {
     return (
       <div>
         {tags.map((tag: Tag) => {
-          return (
+          return links ? (
             <Link key={tag.id} as={`/tag/${tag.slug}`} href="/tag/[slug]" passHref>
               <a className="uk-badge">{tag.name}</a>
             </Link>
+          ) : (
+            <span key={tag.id} className="uk-badge">
+              {tag.name}
+            </span>
           )
         })}
       </div>
