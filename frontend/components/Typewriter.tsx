a3a15f71cd890c15742da67bbf0ddbdc5fde5818
Marko Leinikka <mleinikka@gmail.com>
2021-12-13 20:45:09 +0200

    fix(frontend): layout shifting on mobile because of typewriter

--- frontend/components/Typewriter.tsx
+++ frontend/components/Typewriter.tsx
@@ -7,7 +7,7 @@ interface TypewriterProps {
 
 const Typewriter = ({ strings, delay }: TypewriterProps) => {
   const [arrIndex, setArrIndex] = useState(0)
-  const [stringIndex, setStringIndex] = useState(0)
+  const [strIndex, setStrIndex] = useState(0)
   const [blink, setBlink] = useState(true)
   const [reverse, setReverse] = useState(false)
 
@@ -17,23 +17,23 @@ const Typewriter = ({ strings, delay }: TypewriterProps) => {
       return
     }
 
-    if (stringIndex === strings[arrIndex].length + 1 && arrIndex !== strings.length - 1 && !reverse) {
+    if (strIndex === strings[arrIndex].length + 1 && arrIndex !== strings.length - 1 && !reverse) {
       setReverse(true)
       return
     }
 
-    if (stringIndex === 0 && reverse) {
+    if (strIndex === 0 && reverse) {
       setReverse(false)
       setArrIndex((prev) => prev + 1)
       return
     }
 
     const timeout = setTimeout(() => {
-      setStringIndex((prev) => prev + (reverse ? -1 : 1))
+      setStrIndex((prev) => prev + (reverse ? -1 : 1))
     }, delay)
 
     return () => clearTimeout(timeout)
-  }, [stringIndex, arrIndex, reverse, strings, delay])
+  }, [strIndex, arrIndex, reverse, strings, delay])
 
   // Blinking cursor
   useEffect(() => {
@@ -43,7 +43,15 @@ const Typewriter = ({ strings, delay }: TypewriterProps) => {
     return () => clearTimeout(timeout2)
   }, [blink])
 
-  return <span>{`${strings[arrIndex].substring(0, stringIndex)}${blink ? "|" : " "}`}</span>
+  // Hidden text to combat Cumulative Layout Shift (https://web.dev/cls)
+  return (
+    <>
+      <span style={{ position: "absolute", paddingRight: "10px" }}>
+        {`${strings[arrIndex].substring(0, strIndex)}${blink ? "|" : " "}`}
+      </span>
+      <span style={{ visibility: "hidden" }}>{strings[arrIndex]}</span>
+    </>
+  )
 }
 
 export default Typewriter
