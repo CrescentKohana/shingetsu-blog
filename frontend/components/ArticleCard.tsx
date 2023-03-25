20fc717f773375e464e0136c076e2794e1950b73
Marko Leinikka <mleinikka@gmail.com>
2023-03-25 14:52:47 +0900

    refactor(frontend): Remove unneeded <a>

--- frontend/components/ArticleCard.tsx
+++ frontend/components/ArticleCard.tsx
@@ -11,27 +11,25 @@ interface CardProps {
 
 const ArticleCard = ({ article }: CardProps) => {
   return (
-    <Link href={`/article/${article.slug}`}>
-      <a className="uk-link-reset">
-        <div className="uk-card uk-card-muted">
-          <div className="uk-card-media-top">
-            <ImageWrap className={styles.cardImg} image={article.image} />
-          </div>
-          <div className="uk-card-body">
-            <p id="title" className="uk-text-large">
-              {article.title}
-            </p>
-            <div className="uk-text-meta">
-              <Moment format="MMM Do YYYY">{article.updated || article.published}</Moment>
-            </div>
-            {article.tags && (
-              <div style={{ marginTop: 5 }}>
-                <Tags tags={article.tags} />
-              </div>
-            )}
-          </div>
+    <Link href={`/article/${article.slug}`} className="uk-link-reset">
+      <div className="uk-card uk-card-muted">
+        <div className="uk-card-media-top">
+          <ImageWrap className={styles.cardImg} image={article.image} />
         </div>
-      </a>
+        <div className="uk-card-body">
+          <p id="title" className="uk-text-large">
+            {article.title}
+          </p>
+          <div className="uk-text-meta">
+            <Moment format="MMM Do YYYY">{article.updated || article.published}</Moment>
+          </div>
+          {article.tags && (
+            <div style={{ marginTop: 5 }}>
+              <Tags tags={article.tags} />
+            </div>
+          )}
+        </div>
+      </div>
     </Link>
   )
 }
